f15af76882621e3f5b1ab4d30a9c6e322d8e8691
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2024-01-26 11:39:45 +0200

    Create ProductController.cs

---
+++ IntroductionMVC/Controllers/ProductController.cs
@@ -0,0 +1,90 @@
+﻿using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using IntroductionMVC.Models.Product;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Net.Http.Headers;
+
+namespace IntroductionMVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IEnumerable<ProductViewModel> _products = new List<ProductViewModel>()
+        {
+            new ProductViewModel()
+            {
+                Id = 1,
+                Name = "Cheese",
+                Price = 7.00
+            },
+            new ProductViewModel()
+            {
+                Id = 2,
+                Name = "Ham",
+                Price = 5.50
+            },
+            new ProductViewModel()
+            {
+                Id = 3,
+                Name = "Bread",
+                Price = 1.50
+            }
+        };
+        public IActionResult Index(string? keyword)
+        {
+            if (keyword != null)
+            {
+                var foundProducts = _products
+                    .Where(k => k.Name.ToLower().Contains(keyword.ToLower()));
+                
+                return View(foundProducts);
+            }
+            return View(_products);
+        }
+
+        public IActionResult ById(int id)
+        {
+            var product = _products.FirstOrDefault(p => p.Id == id);
+
+            if (product == null)
+            {
+                return BadRequest();
+            }
+
+            return View(product);
+        }
+
+        public IActionResult AllAsJson()
+        {
+            var options = new JsonSerializerOptions()
+            {
+                WriteIndented = true
+            };
+            return Json(_products, options);
+        }
+
+        public IActionResult AllAsText()
+        {
+            var sb = new StringBuilder();
+            foreach (var product in _products)
+            {
+                sb.AppendLine($"Product {product.Id}: {product.Name} - {product.Price} lv.");
+            }
+
+            return Content(sb.ToString());
+        }
+
+        public IActionResult AllAsTextFile()
+        {
+            var sb = new StringBuilder();
+            foreach (var product in _products)
+            {
+                sb.AppendLine($"Product {product.Id}: {product.Name} - {product.Price} lv.");
+            }
+
+            Response.Headers.Add(HeaderNames.ContentDisposition, @"attachment;filename=products.txt");
+
+            return File(Encoding.UTF8.GetBytes(sb.ToString().TrimEnd()), "text/plain");
+        }
+    }
+}
