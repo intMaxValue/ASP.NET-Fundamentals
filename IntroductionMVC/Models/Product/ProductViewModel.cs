7754459009179023fc736bb857f0e6209ce09091
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2024-01-26 11:40:28 +0200

    Create ProductViewModel.cs

---
+++ IntroductionMVC/Models/Product/ProductViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace IntroductionMVC.Models.Product
+{
+    public class ProductViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public double Price { get; set; }
+    }
+}
