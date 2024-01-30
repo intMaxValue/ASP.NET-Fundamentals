698a7169d9233ac21b18781057e7b486cbbd7b52
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2024-01-30 23:20:31 +0200

    TextSplitter

---
+++ TextSplitterApp/Models/TextViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TextSplitterApp.Models
+{
+    public class TextViewModel
+    {
+        [Required]
+        [StringLength(100, MinimumLength = 2)]
+        public string Text { get; set; } = null!;
+
+        public string SplitText { get; set; } = null!;
+
+    }
+}
