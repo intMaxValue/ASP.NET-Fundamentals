01c09df033eece010ea14e2aaa4daa6d54bab1ac
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2024-02-19 03:04:55 +0200

    Create HomeController.cs

---
+++ Exam Prep/Homies/Homies/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Homies.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Homies.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            if (User?.Identity != null && User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("All", "Event");
+            }
+            return View();
+        }
+
+
+    }
+}
