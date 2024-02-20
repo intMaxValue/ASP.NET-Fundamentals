b763ed4a5313bc28ed4c6ada2e3505c93a965ed5
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2024-02-20 11:34:45 +0200

    Create DataConstants.cs

---
+++ Exam Prep/Homies/Homies/Data/DataConstants.cs
@@ -0,0 +1,21 @@
+﻿namespace Homies.Data
+{
+    public static class DataConstants
+    {
+        // Event
+        public const int EventNameMinLength = 5;
+        public const int EventNameMaxLength = 20;
+
+        public const int EventDescriptionMinLength = 15;
+        public const int EventDescriptionMaxLength = 150;
+
+        public const string DateTimeFormat = "yyyy-MM-dd H:mm";
+
+        public const string LengthErrorMessage = "The field {0} must be between {2} and {1} characters.";
+        public const string RequiredErrorMessage = "The field {0} is required.";
+
+        // Type
+        public const int TypeNameMinLength = 5;
+        public const int TypeNameMaxLength = 15;
+    }
+}
