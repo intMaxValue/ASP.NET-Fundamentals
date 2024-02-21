89e373bc28b88afa41f17c8cc3a09bfc2bd0408b
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2024-02-21 23:29:59 +0200

    Create Event.cs

---
+++ Exam Prep/Homies/Homies/Data/Models/Event.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
+using static Homies.Data.DataConstants;
+
+namespace Homies.Data.Models
+{
+    public class Event
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(EventNameMaxLength)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(EventDescriptionMaxLength)]
+        public string Description { get; set; } = string.Empty;
+
+        [Required] 
+        public string OrganiserId { get; set; } = string.Empty;
+
+        [ForeignKey(nameof(OrganiserId))]
+        public IdentityUser Organiser { get; set; } = null!;
+
+        [Required]
+        public DateTime CreatedOn { get; set; }
+
+        [Required]
+        public DateTime Start { get; set;}
+
+        [Required] 
+        public DateTime End { get; set; }
+
+        [Required]
+        public int TypeId { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(TypeId))]
+        public Type Type { get; set; } = null!;
+
+        public ICollection<EventParticipant> EventsParticipants { get; set; } = new List<EventParticipant>();
+    }
+}
