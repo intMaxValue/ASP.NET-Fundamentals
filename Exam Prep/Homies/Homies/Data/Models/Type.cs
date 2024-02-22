eea4af1e60c2c926b5f969a39f340fc3aefd4868
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2024-02-22 11:50:50 +0200

    Create Type.cs

---
+++ Exam Prep/Homies/Homies/Data/Models/Type.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+using static Homies.Data.DataConstants;
+
+namespace Homies.Data.Models
+{
+    public class Type
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(TypeNameMaxLength)]
+        public string Name { get; set; } = string.Empty;
+
+        public ICollection<Event> Events { get; set; } = new List<Event>();
+    }
+}
