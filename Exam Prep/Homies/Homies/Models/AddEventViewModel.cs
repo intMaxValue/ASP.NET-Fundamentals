c74a99fd4a2e39e00db0081e7252de8281a9cf3f
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2024-02-23 16:51:04 +0200

    Create AddEventViewModel.cs

---
+++ Exam Prep/Homies/Homies/Models/AddEventViewModel.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using Homies.Data;
+using static Homies.Data.DataConstants;
+using Type = Homies.Data.Models.Type;
+
+namespace Homies.Models
+{
+    public class AddEventViewModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = RequiredErrorMessage)]
+        [StringLength(EventNameMaxLength, MinimumLength = EventNameMinLength,
+            ErrorMessage = LengthErrorMessage)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = RequiredErrorMessage)]
+        [StringLength(EventDescriptionMaxLength, MinimumLength = EventDescriptionMinLength,
+            ErrorMessage = LengthErrorMessage)]
+        public string Description { get; set; } = string.Empty;
+
+        
+        [Required]
+        public DateTime CreatedOn { get; set; }
+
+        [Required]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd H:mm}", ApplyFormatInEditMode = true)]
+        public DateTime Start { get; set; }
+
+        [Required]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd H:mm}", ApplyFormatInEditMode = true)]
+        public DateTime End { get; set; }
+
+        [Required]
+        public int TypeId { get; set; }
+
+        public string? OrganiserId { get; set; } = string.Empty;
+        public ICollection<TypeViewModel> Types { get; set; } = new List<TypeViewModel>();
+    }
+}
