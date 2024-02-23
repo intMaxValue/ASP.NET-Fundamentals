2fef4d19f55e766438566d0bad84d8e6f9d04d49
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2024-02-23 16:51:31 +0200

    Create AllEventsViewModel.cs

---
+++ Exam Prep/Homies/Homies/Models/AllEventsViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+using static Homies.Data.DataConstants;
+
+namespace Homies.Models
+{
+    public class AllEventsViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Start { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Type { get; set; } = string.Empty;
+        public string Organiser { get; set; } = string.Empty;
+    }
+}
