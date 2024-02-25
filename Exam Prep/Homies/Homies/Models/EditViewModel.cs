a747ea3e176450ce6dd2e9d2796dd445d240eda7
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2024-02-25 09:22:28 +0200

    Create EditViewModel.cs

---
+++ Exam Prep/Homies/Homies/Models/EditViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+using static Homies.Data.DataConstants;
+
+namespace Homies.Models
+{
+    public class EditViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(EventNameMaxLength, MinimumLength = EventNameMinLength,
+            ErrorMessage = LengthErrorMessage)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(EventDescriptionMaxLength, MinimumLength = EventDescriptionMinLength,
+            ErrorMessage = LengthErrorMessage)]
+        public string Description { get; set; } = string.Empty;
+
+        public DateTime Start { get; set; }
+
+        public DateTime End { get; set; }
+
+        public int TypeId { get; set; }
+
+        public ICollection<TypeViewModel> Types { get; set; } = new List<TypeViewModel>();
+    }
+}
