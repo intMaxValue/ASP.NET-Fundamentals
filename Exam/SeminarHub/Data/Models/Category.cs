441531c10a371d62672f50faa455773f02fc361a
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2024-03-22 20:15:38 +0200

    Create Category.cs

---
+++ Exam/SeminarHub/Data/Models/Category.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+using static SeminarHub.Data.DataConstants;
+
+namespace SeminarHub.Data.Models
+{
+    [Comment("Category model")]
+    public class Category
+    {
+        [Key]
+        [Comment("Category Identifier")]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(CategoryNameMaxLength, MinimumLength = CategoryNameMinLength,
+            ErrorMessage = LengthErrorMessage)]
+        [Comment("Category Name")]
+        public string Name { get; set; } = string.Empty;
+
+        [Comment("Collection of seminars")]
+        public ICollection<Seminar> Seminars { get; set; } = new List<Seminar>();
+    }
+}
