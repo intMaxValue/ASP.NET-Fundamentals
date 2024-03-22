d69d6d1140f70c757d65e85032f340124cfd0097
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2024-03-22 20:16:01 +0200

    Create SeminarParticipant.cs

---
+++ Exam/SeminarHub/Data/Models/SeminarParticipant.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace SeminarHub.Data.Models
+{
+    [Comment("Seminar Participants mapping table")]
+    public class SeminarParticipant
+    {
+        [Key]
+        [Comment("Seminar Identifier")]
+        public int SeminarId { get; set; }
+
+        [ForeignKey(nameof(SeminarId))]
+        [Comment("Foreign key of the Seminar")]
+        public Seminar Seminar { get; set; } = null!;
+
+        [Key]
+        [Comment("Participant Identifier")]
+        public string ParticipantId { get; set; } = string.Empty;
+
+        [ForeignKey(nameof(ParticipantId))]
+        [Comment("Foreign key of the participant")]
+        public IdentityUser Participant { get; set; } = null!;
+    }
+}
