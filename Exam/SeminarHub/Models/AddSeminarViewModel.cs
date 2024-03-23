3d457ebc90c9e09b3ced63e03620733ac77cbf1b
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2024-03-23 23:07:37 +0200

    Create AddSeminarViewModel.cs

---
+++ Exam/SeminarHub/Models/AddSeminarViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+using static SeminarHub.Data.DataConstants;
+
+namespace SeminarHub.Models
+{
+    public class AddSeminarViewModel
+    {
+        [Required]
+        [StringLength(SeminarTopicMaxLength, MinimumLength = SeminarTopicMinLength,
+            ErrorMessage = LengthErrorMessage)]
+        public string Topic { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(SeminarLecturerMaxLength, MinimumLength = SeminarLecturerMinLength,
+            ErrorMessage = LengthErrorMessage)]
+        public string Lecturer { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(SeminarDetailsMaxLength, MinimumLength = SeminarDetailsMinLength,
+            ErrorMessage = LengthErrorMessage)]
+        public string Details { get; set; } = string.Empty;
+
+        public string OrganizerId { get; set; } = string.Empty;
+
+
+        [Required]
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(DataFormatString = DateTimeFormat, ApplyFormatInEditMode = true)]
+        public DateTime DateAndTime { get; set; }
+
+        [Range(SeminarDurationMinLength, SeminarDurationMaxLength)]
+        public int Duration { get; set; }
+
+        [Required]
+        public int CategoryId { get; set; }
+
+        public ICollection<CategoriesViewModel> Categories { get; set; } = new List<CategoriesViewModel>();
+    }
+}
