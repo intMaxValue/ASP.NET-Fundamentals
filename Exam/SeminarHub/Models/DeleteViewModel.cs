1d5c088f42b5e65fac2d557cc580bdfb243db36d
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2024-03-24 23:05:16 +0200

    Create DeleteViewModel.cs

---
+++ Exam/SeminarHub/Models/DeleteViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace SeminarHub.Models
+{
+    public class DeleteViewModel
+    {
+        public int Id { get; set; }
+
+        public string Topic { get; set; } = string.Empty;
+
+        public DateTime DateAndTime { get; set; }
+
+
+    }
+}
