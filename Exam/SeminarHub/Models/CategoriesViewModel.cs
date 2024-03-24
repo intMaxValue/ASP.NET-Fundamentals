a8e85a8ca7271dd08258ed2d985116e39f499197
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2024-03-24 23:05:04 +0200

    Create CategoriesViewModel.cs

---
+++ Exam/SeminarHub/Models/CategoriesViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace SeminarHub.Models
+{
+    public class CategoriesViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+    }
+}
