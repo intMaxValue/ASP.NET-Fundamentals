7ae0e797c8c0ffc87135e9cfe9941175a8bac890
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2024-03-24 09:26:38 +0200

    Create AllSeminarsViewModel.cs

---
+++ Exam/SeminarHub/Models/AllSeminarsViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace SeminarHub.Models
+{
+    public class AllSeminarsViewModel
+    {
+        public int Id { get; set; }
+        public string Topic { get; set; } = string.Empty;
+
+        public string Lecturer { get; set; } = string.Empty;
+
+        public string Category { get; set; } = string.Empty;
+
+        public DateTime DateAndTime { get; set; }
+
+        public string Organizer { get; set; } = string.Empty;
+    }
+}
