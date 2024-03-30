17ff4d51f8425205e8527320200fcb9e55939247
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2024-03-30 22:00:36 +0200

    Create JoinedSeminarsViewModel.cs

---
+++ Exam/SeminarHub/Models/JoinedSeminarsViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace SeminarHub.Models
+{
+    public class JoinedSeminarsViewModel
+    {
+        public int Id { get; set; }
+
+        public string Topic { get; set; } = string.Empty;
+
+        public string Lecturer { get; set; } = string.Empty;
+
+        public DateTime DateAndTime { get; set; }
+
+        public string Organizer { get; set; } = string.Empty;
+    }
+}
